8736e980d7ba4538d06012779ba8899ffebfa6ac
augusto-kenzo <ak47lanfredi@hotmail.com>
2024-09-09 22:57:12 -0300

    Add files via upload

--- EXERCICIO 2.c
+++ EXERCICIO 2.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-    int i, n;
+int main() {
+    int n, i, primo = 1;
 
-    printf("digite o numero inicial:");
-    scanf("%d",&n);
-    if(n<=1){
-        printf("não é primo");
-    }else{
-        for (i=2; i < n; i++){
-            if(n%i==0){
-                printf("nao e primo");
-                break;
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("O numero nao e primo.\n");
+        return 0;
+    }
+
+    for (i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            primo = 0;
+            break;
+        }
+    }
+
+    if (primo) {
+        printf("O numero %d e primo.\n", n);
+    } else {
+        printf("O numero %d nao e primo.\n", n);
+        printf("Divisores de %d: 1", n);
+        for (i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                printf(", %d", i);
             }
         }
-        if(i==n){
-
-            printf("e primo");
-        }
-
+        printf("\n");
     }
+
     return 0;
 }
+
